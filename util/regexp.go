3b3a761a900ec2b0c15d2bcf8e35c072d5f007cf
tan90 <tan90indream@163.com>
2017-11-27 03:52:27 +0800

    password regexp

--- util/regexp.go
+++ util/regexp.go
@@ -4,14 +4,25 @@ import (
 	"regexp"
 )
 
-var emailRegExp *regexp.Regexp
+var emailRegExp, pwdRegExp *regexp.Regexp
 
 func init() {
+	// emailRegExp 定义了电子邮件的格式
+	// See: https://github.com/StefanSchroeder/Golang-Regex-Tutorial/blob/master/zh/01-chapter3.markdown#验证电子邮件地址
 	emailRegExp, _ = regexp.Compile(`\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3}$`)
+
+	// pwdRegExp 定义了密码
+	// 最小8位 最大20位
+	// 只允许大小写 数字 下划线
+	pwdRegExp, _ = regexp.Compile(`\w{8,20}$`)
 }
 
 // IsEmail 验证是否是电子邮件
-// See: https://github.com/StefanSchroeder/Golang-Regex-Tutorial/blob/master/zh/01-chapter3.markdown#验证电子邮件地址
 func IsEmail(s string) bool {
 	return emailRegExp.MatchString(s)
+}
+
+// IsPwd 验证是否是密码
+func IsPwd(s string) bool {
+	return pwdRegExp.MatchString(s)
 }
--- util/regexp_test.go
+++ util/regexp_test.go
@@ -20,8 +20,27 @@ var emailTestCases = []struct {
 
 func TestIsEmail(t *testing.T) {
 	for _, tc := range emailTestCases {
-		Convey(tc.testcase+tc.is+" a email address.", t, func() {
+		Convey(tc.testcase+tc.is+" an email address.", t, func() {
 			So(util.IsEmail(tc.testcase), ShouldEqual, tc.result)
+		})
+	}
+}
+
+var pwdTestCases = []struct {
+	testcase string
+	is       string
+	result   bool
+}{
+	{"fizzBuzz@gmail.com", " is not", false},
+	{"foobar2000", " is", true},
+	{"no#@O!UBD!Da", " is not", false},
+	{"2333_wysiwyg", " is", true},
+}
+
+func TestIsPwd(t *testing.T) {
+	for _, tc := range pwdTestCases {
+		Convey(tc.testcase+tc.is+" a valid password.", t, func() {
+			So(util.IsPwd(tc.testcase), ShouldEqual, tc.result)
 		})
 	}
 }
